d7e026d1cd29022303d930a43f5460410a9b22e1
gtraines <gtraines@hotmail.com>
2019-01-12 18:23:54 -0700

    blah

--- stately/state.py
+++ stately/state.py
@@ -1,4 +1,4 @@
-from __future__ import absolute_import
+from __future__ import absolute_import, print_function
 from .g13_handler import G13Keys
 
 """Encompasses the state stack as given to the plugins.
@@ -7,102 +7,105 @@ Plugins get registered for various actions in each state they wish. This object
 is then passed into plugins that are triggered for those actions.
 """
 
+
 class PluginState(object):
-  def __init__(self, handler, action_helper):
-    self.handler = handler
-    self.states = {}
-    self.stack = []
+    def __init__(self, handler, action_helper):
+        self.handler = handler
+        self.states = {}
+        self.stack = []
 
-    self.action = action_helper
+        self.action = action_helper
 
-  @property
-  def current_state(self):
-    return self.states[self.current_state_name] if self.stack else None
-  @property
-  def current_state_name(self):
-    return self.stack[-1] if self.stack else None
+    @property
+    def current_state(self):
+        return self.states[self.current_state_name] if self.stack else None
 
-  def _register_action(self, state, actions, action_name):
-    action = actions.get(action_name)
-    if action:
-      state[action_name] = state.get(action_name, [])
-      state[action_name].append(action)
+    @property
+    def current_state_name(self):
+        return self.stack[-1] if self.stack else None
 
-  def _register_key_action(self, state, actions, key_action_name):
-    key_action = actions.get(key_action_name)
-    if key_action:
-      state[key_action_name] = state.get(key_action_name, {})
-      for key in G13Keys.keys:
-        if key not in key_action:
-          continue
-        if key in state[key_action_name]:
-          state[key_action_name][key].append(key_action[key])
-        else:
-          state[key_action_name][key] = [key_action[key]]
+    def _register_action(self, state, actions, action_name):
+        action = actions.get(action_name)
+        if action:
+            state[action_name] = state.get(action_name, [])
+            state[action_name].append(action)
 
-  def register_plugin(self, states={}):
-    for state_name, actions in states.items():
-      if state_name not in self.states:
-        self.states[state_name] = {}
-      state = self.states[state_name]
+    def _register_key_action(self, state, actions, key_action_name):
+        key_action = actions.get(key_action_name)
+        if key_action:
+            state[key_action_name] = state.get(key_action_name, {})
+            for key in G13Keys.keys:
+                if key not in key_action:
+                    continue
+                if key in state[key_action_name]:
+                    state[key_action_name][key].append(key_action[key])
+                else:
+                    state[key_action_name][key] = [key_action[key]]
 
-      self._register_action(state, actions, 'enter')
-      self._register_action(state, actions, 'exit')
-      self._register_action(state, actions, 'joystick')
+    def register_plugin(self, states=None):
+        if states is None:
+            states = {}
+        for state_name, actions in states.items():
+            if state_name not in self.states:
+                self.states[state_name] = {}
+            state = self.states[state_name]
 
-      self._register_key_action(state, actions, 'key_press')
-      self._register_key_action(state, actions, 'key_release')
+            self._register_action(state, actions, 'enter')
+            self._register_action(state, actions, 'exit')
+            self._register_action(state, actions, 'joystick')
 
-  def handle_state_event(self, event):
-    handlers = self.current_state.get(event, [])
-    for handler in handlers:
-      handler(self, self.current_state)
+            self._register_key_action(state, actions, 'key_press')
+            self._register_key_action(state, actions, 'key_release')
 
-  def enter_state(self, state_name):
-    if state_name == self.current_state_name:
-      return
+    def handle_state_event(self, event):
+        handlers = self.current_state.get(event, [])
+        for handler in handlers:
+            handler(self, self.current_state)
 
-    self.stack.append(state_name)
-    if state_name not in self.states:
-      print 'WARNING: Entering unregistered state:', state_name
-      self.states[state_name] = {}
-    self.handle_state_event('enter')
+    def enter_state(self, state_name):
+        if state_name == self.current_state_name:
+            return
 
-  def exit_state(self, state_name):
-    # Only pop off states.
-    if state_name != self.current_state_name:
-      return
+        self.stack.append(state_name)
+        if state_name not in self.states:
+            print('WARNING: Entering unregistered state:', state_name)
+            self.states[state_name] = {}
+        self.handle_state_event('enter')
 
-    self.handle_state_event('exit')
-    self.stack.remove(state_name)
+    def exit_state(self, state_name):
+        # Only pop off states.
+        if state_name != self.current_state_name:
+            return
 
-  def key_changed(self, key, is_pressed):
-    local_stack = list(self.stack)
-    while local_stack:
-      state = local_stack.pop()
-      if is_pressed:
-        keys = self.states[state].get('key_press', {})
-      else:
-        keys = self.states[state].get('key_release', {})
+        self.handle_state_event('exit')
+        self.stack.remove(state_name)
 
-      funcs = keys.get(key)
-      if not funcs:
-        # If nothing's registered, try the previous state.
-        continue
-      [func(self, key) for func in funcs]
-      break
+    def key_changed(self, key, is_pressed):
+        local_stack = list(self.stack)
+        while local_stack:
+            state = local_stack.pop()
+            if is_pressed:
+                keys = self.states[state].get('key_press', {})
+            else:
+                keys = self.states[state].get('key_release', {})
 
-  def joystick(self, stick_x, stick_y):
-    # Similar to key_changed, but collapsing them will cause a huge
-    # hit to readability since joystick isn't a dict while
-    # key_press/release is.
-    local_stack = list(self.stack)
-    while local_stack:
-      state = local_stack.pop()
-      funcs = self.states[state].get('joystick', [])
+            funcs = keys.get(key)
+            if not funcs:
+                # If nothing's registered, try the previous state.
+                continue
+            [func(self, key) for func in funcs]
+            break
 
-      if not funcs:
-        continue
-      [func(self, stick_x, stick_y) for func in funcs]
-      break
+    def joystick(self, stick_x, stick_y):
+        # Similar to key_changed, but collapsing them will cause a huge
+        # hit to readability since joystick isn't a dict while
+        # key_press/release is.
+        local_stack = list(self.stack)
+        while local_stack:
+            state = local_stack.pop()
+            funcs = self.states[state].get('joystick', [])
 
+            if not funcs:
+                continue
+            [func(self, stick_x, stick_y) for func in funcs]
+            break
